03198175193b21bd390ded50fb921b1af79eb320
unknown <brewerkberly@gmail.com>
2022-07-21 04:41:50 -0700

    Token parm change

--- market-contract/src/external.rs
+++ market-contract/src/external.rs
@@ -9,9 +9,9 @@ trait ExtContract {
     fn nft_transfer_payout(
         &mut self,
         receiver_id: AccountId, //purchaser (person to transfer the NFT to)
-        token_id: TokenId, //token ID to transfer
+        token_id: TokenId,
         approval_id: u64, //market contract's approval ID in order to transfer the token on behalf of the owner
-        memo: String, //memo (to include some context)
+        memo: String, 
         /*
             the price that the token was purchased for. This will be used in conjunction with the royalty percentages
             for the token in order to determine how much money should go to which account. 
